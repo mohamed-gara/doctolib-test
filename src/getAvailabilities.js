d7bfa62775c2d5c76ce5374910a9f472ba56502a
jb cazaux <jbcazaux@gmail.com>
2018-06-23 00:33:25 +0200

    first commit

---
+++ src/getAvailabilities.js
@@ -0,0 +1,6 @@
+import moment from 'moment'
+import knex from 'knexClient'
+
+export default async function getAvailabilities(date) {
+  // Implement your algorithm here
+}
---
+++ src/getAvailabilities.test.js
@@ -0,0 +1,50 @@
+import knex from 'knexClient'
+import getAvailabilities from './getAvailabilities'
+
+describe('getAvailabilities', () => {
+  beforeEach(() => knex('events').truncate())
+
+  describe('simple case', () => {
+    beforeEach(async () => {
+      await knex('events').insert([
+        {
+          kind: 'opening',
+          starts_at: new Date('2014-08-04 09:30'),
+          ends_at: new Date('2014-08-04 12:30'),
+          weekly_recurring: true,
+        },
+        {
+          kind: 'appointment',
+          starts_at: new Date('2014-08-11 10:30'),
+          ends_at: new Date('2014-08-11 11:30'),
+        },
+      ])
+    })
+
+    it('should fetch availabilities correctly', async () => {
+      const availabilities = await getAvailabilities(new Date('2014-08-10'))
+      expect(availabilities.length).toBe(7)
+
+      expect(String(availabilities[0].date)).toBe(
+        String(new Date('2014-08-10')),
+      )
+      expect(availabilities[0].slots).toEqual([])
+
+      expect(String(availabilities[1].date)).toBe(
+        String(new Date('2014-08-11')),
+      )
+      expect(availabilities[1].slots).toEqual([
+        '9:30',
+        '10:00',
+        '11:30',
+        '12:00',
+      ])
+
+      expect(availabilities[2].slots).toEqual([])
+
+      expect(String(availabilities[6].date)).toBe(
+        String(new Date('2014-08-16')),
+      )
+    })
+  })
+})
